5a5821ac5f0ca873186bd3733356657e465a3fb3
vitkuzmenko <kuzmenko.v.u@gmail.com>
2014-06-14 02:17:22 +0400

    Fixed Table

--- TableControl.php
+++ TableControl.php
@@ -23,15 +23,31 @@ class TableControl extends OwnedControl {
 	protected $isFilled;
 	
 	public function __construct($parent, $cols, $rows, $autoFill = true, $headRow = -1) {
-		parent::__construct($parent, $tag);
+		parent::__construct($parent, 'table');
 		
-		$this->headRow = $headrow;
+		$this->headRow = $headRow;
 		$this->cols = $cols;
 		$this->rows = $rows;
 		
 		if ($autoFill) {
 			$this->fillTable();
 		}
+	}
+	
+	public function setBorder($value) {
+		$this->setAttr('border', $value);
+	}
+	
+	public function setWidth($value) {
+		$this->setAttr('width', $value);
+	}
+	
+	public function setCellPadding($value) {
+		$this->setAttr('cellpadding', $value);
+	}
+
+	public function setCellSpacing($value) {
+		$this->setAttr('cellspacing', $value);
 	}
 	
 	protected function createRow($isHead = false) {
@@ -52,7 +68,7 @@ class TableControl extends OwnedControl {
 			}
 		}
 		
-		$this->isfilled = true;
+		$this->isFilled = true;
 	}
 
 	public function addRow(array $value = array(), $isHeading = false) {
@@ -71,17 +87,19 @@ class TableControl extends OwnedControl {
 	
 	public function row($rowId) {
 		if (!$this->isFilled) {
-			return false;
+			return null;
 		}
 		
 		$rows = $this->getControls('tr');
 		
-		return $rows[$rowid];
+		return $rows[$rowId];
 	}
 	
 	public function fillRow($rowId, array $value = array()) {
 		$row = $this->row($rowId);
-		$row->fill($value);
+		if ($row) {
+			$row->fill($value);
+		}
 	}
 	
 	public function cell($col, $row) {
