32cdb581dc85cbb0e921501e0dd76c6df6bab200
vitkuzmenko <kuzmenko.v.u@gmail.com>
2014-06-18 19:01:56 +0400

    Example Update

--- example/index.php
+++ example/index.php
@@ -8,35 +8,17 @@ class index extends \Bootstrap\Document {
 		parent::__construct('hello', 'utf-8', 'en');
 	
 		$body = $this->body;
-
-		$form = $body->addForm(1, 'myForm', '/');
-
-		$form->setFormInline(true, false);
-		$form->setFormHorizontal();
-		$form->setHorizontalLabelOffset(2);
 		
-		$form->addFormGroupWithLabelTextFieldAndHelpBlock('myGroup', 'My Field', 'Placeholder', null, 'Help Block');
+		$dropdown = new \Bootstrap\Dropdown($body);
+		$dropdown->initButton('hello');
 		
-		$successGroup = $form->addFormGroupWithLabelAndTextField('myGroup', 'My Field', 'Placeholder');
-		$successGroup->setSuccessState(true);
+		$dropdown->addHeaderItem('Header');
+		$dropdown->addItem('First Item', '#');
+		$dropdown->addItem('Second Item', '#', true);
+		$dropdown->addDivider();
+		$dropdown->addItem('Third Item', '#', false, true);
 		
-		$warningGroup = $form->addFormGroupWithLabelAndTextField('myGroup', 'My Field', 'Placeholder');
-		$warningGroup->setWarningState(true);
-		
-		$errorGroup = $form->addFormGroupWithLabelAndTextField('myGroup', 'My Field', 'Placeholder');
-		$errorGroup->setErrorState(true);
-		
-		$form->addFormGroupWithSelectField('mySelect', 'My Select', array(1, 2, 3));
-		
-		$form->addFormGroupWithCheckBox('myCheckBox', 'My Checkbox');
-		
-		$form->addFormGroupWithRadioButton('myRadioButton', 'My Radio Button');
-		$form->addFormGroupWithRadioButton('myRadioButton', 'My Radio Button', null, true);
-		$form->addFormGroupWithRadioButton('myRadioButton', 'My Radio Button');
-		
-		$button = $form->addButton('My Button', 'myButton', 'info');
-		$button->setDangerStyle();
-		
+		$body->addControl($dropdown);
 	}
 	
 }
